cf9d90429ce697150b8589b52ce9a1841dc4c198
Dev Borland <devborland@gmail.com>
2020-12-14 11:59:38 +0300

    Add const for input text decoration

--- lib/components/rounded_button.dart
+++ lib/components/rounded_button.dart
@@ -19,6 +19,7 @@ class RoundedButton extends StatelessWidget {
           minWidth: 200.0,
           height: 42.0,
           child: Text(title),
+          textColor: Colors.white,
         ),
       ),
     );
