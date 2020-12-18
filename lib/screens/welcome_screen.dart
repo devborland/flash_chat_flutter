03faf36c7a094bc026335109008be1311dae2d88
Dev Borland <devborland@gmail.com>
2020-12-18 11:47:20 +0300

    Prepare send and receive messages from Firestore

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -36,7 +36,7 @@ class _WelcomeScreenState extends State<WelcomeScreen>
     controller.forward();
     controller.addListener(() {
       setState(() {});
-      print(animation.value);
+      // print(animation.value);
     });
   }
 
